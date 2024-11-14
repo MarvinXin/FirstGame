0d661879a76c0c1e4fb83abbfbad87ee88c98ddc
MarvinXin <marvin.j.xin@gmail.com>
2024-11-14 13:52:14 -0800

    Removed unnecessary code
    Removed body code

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -3,6 +3,6 @@ extends Area2D
 @onready var game_manager: Node = %"Game Manager"
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
-func _on_body_entered(body: Node2D) -> void:
+func _on_body_entered(_body: Node2D) -> void:
 	game_manager.add_points() # Replace with function body.
 	animation_player.play("pickup")
--- scripts/kill_zone.gd
+++ scripts/kill_zone.gd
@@ -3,7 +3,7 @@ extends Area2D
 @onready var timer: Timer = $Timer
 
 
-func _on_body_entered(body: Node2D) -> void:
+func _on_body_entered(_body: Node2D) -> void:
 	print("You've Died!")
 	Engine.time_scale = 0.5
 	#body.get_node("CollisionShape2D").queue_free()
